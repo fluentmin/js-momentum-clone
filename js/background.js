2f2db1f7f4d91a939e33305bb62c6458906ccb98
fluentmin <fluentmin@gmail.com>
2022-02-20 23:54:28 +0900

    Background Image
    fixed Background images part

--- js/background.js
+++ js/background.js
@@ -1,9 +1,4 @@
-const images = [
-    "https://i0.wp.com/512pixels.net/downloads/macos-wallpapers-thumbs/10-7--thumb.png?w=640&ssl=1",
-     "https://i0.wp.com/512pixels.net/downloads/macos-wallpapers-thumbs/10-10--thumb.jpg?w=640&ssl=1",
-      "https://i0.wp.com/512pixels.net/downloads/macos-wallpapers-thumbs/10-13--thumb.jpg?w=640&ssl=1",
-      "https://i0.wp.com/512pixels.net/downloads/macos-wallpapers-thumbs/10-9--thumb.jpg?w=640&ssl=1"
-];
+const images = ["1.jpeg", "2.jpeg", "3.jpeg"];
 
 const chosenImage = images[Math.floor(Math.random() * quotes.length)];
 
@@ -13,6 +8,6 @@ const chosenImage = images[Math.floor(Math.random() * quotes.length)];
 
 //document.body.appendChild(bgImage);
 
-document.body.style.background = `url(${chosenImage})`
+document.body.style.background = `url(images/${chosenImage})`
 document.body.style.backgroundSize = "cover";
 document.body.style.backgroundPosition = "center";
